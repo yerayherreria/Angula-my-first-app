a4a0099741838ca9cb3e54bfadd288b138e4850f
Yeray Herrería Oña <yerayherre@gmail.com>
2023-12-15 16:22:07 +0100

    contador acabado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { SaludoComponent } from '../saludo/saludo.component';
 import { ContadorComponent } from './contador/contador.component';
 import { ListadoComponent } from './listado/listado.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SaludoComponent, ContadorComponent,ListadoComponent],
+  imports: [CommonModule, RouterOutlet, SaludoComponent, ContadorComponent,ListadoComponent,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -16,4 +17,8 @@ export class AppComponent {
   title = 'angularFirst';
   counter:number = 0;
   increment:number=1;
+
+  updateIncrement(number:string){
+    this.increment=parseInt(number);
+  }
 }
